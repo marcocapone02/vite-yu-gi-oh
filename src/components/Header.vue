771efe23d58e355a33ed69ae7cc6222444a85e6b
Marco Capone <marcocapone02@gmail.com>
2023-05-09 22:04:32 +0200

    Card

---
+++ src/components/Header.vue
@@ -0,0 +1,20 @@
+<script>
+export default {
+  name: 'Header'
+}
+</script>
+
+<template>
+  <div class="header">
+    <span>Yu-Gi-Oh Api</span>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+@use '../scss/partials/generals' as *;
+@use '../scss/partials/variables' as *;
+
+.header{
+  height: 30px;
+}
+</style>
